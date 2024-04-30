bec91f0605ea4ecc9ce0717a34fc747aef135cf6
Eduardo Schiliga <103791066+Eduschiliga@users.noreply.github.com>
2024-04-30 09:20:31 -0300

    Add Lombok

--- src/main/java/com/eduschiliga/todosimple/models/Task.java
+++ src/main/java/com/eduschiliga/todosimple/models/Task.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
-import java.util.Objects;
+import lombok.*;
 
 @Entity
-@Table(name = "Task.TABLE_NAME")
-
+@Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -22,65 +25,11 @@ public class Task {
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
-    @Column(name = "description", length = 255, nullable = false)
+    @Column(name = "description", nullable = false)
     @NotNull
     @NotEmpty
     @Size(min = 1, max = 255)
     private String description;
 
-    public Task() { }
 
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Task other = (Task) obj;
-        if (this.id == null) {
-            if (other.id != null) {
-                return false;
-            } else if (!this.id.equals(other.id)) {
-                return false;
-            }
-        }
-        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) && Objects.equals(this.description, other.description);
-
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
 }
